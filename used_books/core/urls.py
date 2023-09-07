98158acd1796ec5de26103ff0e8f200634c329b3
edieski <eden.skilbeckgaborit@gmail.com>
2023-09-07 21:29:15 +0200

    made the db and finished the base html

--- used_books/core/urls.py
+++ used_books/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.index, name='index')
+    path('home', views.index, name='index'), 
+    path('contact/', views.contact, name = 'contact') 
 ]
