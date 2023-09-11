a7fc9d90e3d46389f526db9ef5f18c953e2a86de
edieski <eden.skilbeckgaborit@gmail.com>
2023-09-11 23:55:47 +0200

    finished index page and item page

--- used_books/used_books/urls.py
+++ used_books/used_books/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include("core.urls"))
+    path('core/', include("core.urls")),
+    path('items/', include("items.urls")),
+
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
