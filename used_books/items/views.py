6dcbaab56a1eda3cce89f6e055dac7270c8beac2
Eden <112548692+edieski@users.noreply.github.com>
2023-09-19 16:44:08 +0200

    Update views.py

--- used_books/items/views.py
+++ used_books/items/views.py
@@ -1,31 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
 # Create your views here.
-from .models import *
-from .forms import NewItemForm
-
-
-def items(request):
-    query = request.GET.get('query', '')
-    category_id = request.GET.get('category', 0)
-    categories = Category.objects.all()
-    items = Item.objects.filter(is_sold=False)
-    
-    if category_id:
-        items = items.filter(category__id=category_id)
-        
-    if query:
-        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query)).distinct()
-    
-    return render(request, 'items/items.html', {
-        'items':items,
-        'query':query,
-        'categories':categories,
-        'category_id':int(category_id),
-        })
-
-
+from .models import Item
+from .forms import NewItemForm, EditItemForm
 
 def detail(request, pk):
 
@@ -53,6 +30,33 @@ def new(request):
             return redirect('items:detail', pk=item.id)
     else:
         form = NewItemForm()
+
+    return render(request, 'items/form.html',  
+                  {'form' : form,
+                           'title': 'New item', })
+
+@login_required
+def delete(request, item_id):
+    item = get_object_or_404(Item, pk=item_id, created_by=request.user)
+    item.delete()
+    return redirect('dashboard:dashboard')
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+    
+        form = NewItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            files = request.FILES  # multivalued dict
+            image = files.get("image")
+            item.image = image
+            item.save()
+
+            return redirect('items:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
 
     return render(request, 'items/form.html',  
                   {'form' : form,
